1785e6001d5394383191db525e93193ef7a916ae
ChloeArd <chloe.ardoise@gmail.com>
2021-11-17 10:13:08 +0100

    Webpack images

--- assets/front.js
+++ assets/front.js
@@ -4,13 +4,18 @@ import * as $ from "jquery";
 import "bootstrap";
 import Chart from "chart.js/auto";
 import {getRelativePosition} from "chart.js/helpers";
-import css from "./css/front.css";
+import "./css/front.css";
+import image from "./images/spiderman-meme.jpg";
 
 sayHello("Chlochlo !");
 
 const say = new SayClass();
 say.sayHello();
 say.sayGoodbye();
+
+const img = document.createElement('img');
+img.src = image;
+document.body.appendChild(img);
 
 $("body").css("color", "#FE0055");
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const webpack = require("webpack");
+require("webpack");
 const path = require("path"); // Module Node utilisable avec Webpack, sert a résoudre les chemins relatifs
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizePlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     // Point d'entrée JS, fichier qui contiendra vos includes
@@ -12,8 +13,9 @@ module.exports = {
     },
     // Objet contenant le chemin de sortie, ainsi que le nom à donner au fichier
     output: {
-        path: path.resolve(__dirname, "public/build/js"),
-        filename: "[name].js"
+        path: path.resolve(__dirname, "public/build"),
+        filename: "js/[name].js",
+        publicPath: "/"
     },
 
     module: {
@@ -30,12 +32,24 @@ module.exports = {
                     {
                         loader: "css-loader",
                         options: {
-                            import: true,
-                            url: true,
+                            url: false,
                             sourceMap: true
                         }
                     }
                 ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader : "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images/",
+                            publicPath: "build/images/"
+                        }
+                    }
+                ]
             }
         ]
     },
@@ -46,7 +60,12 @@ module.exports = {
         ]
     },
 
-    plugins: [].concat([new MiniCssExtractPlugin({
-        filename: "../css/[name].css"
-    })]),
+    plugins: [].concat([
+        new MiniCssExtractPlugin({filename: "css/[name].css"}),
+        new CopyPlugin({
+            patterns: [
+                {from: "assets/css/images", to: "images/"}
+            ]
+        }),
+    ]),
 };
